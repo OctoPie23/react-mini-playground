431ddead5d78e023957e85c70c85e7bd3469e0a6
Shrijal Acharya <coveringguy0123@gmail.com>
2023-03-27 20:41:25 +0545

    Add the info section to the digital card

---
+++ digital-card/src/components/Handles.tsx
@@ -0,0 +1,9 @@
+const Handles = () => {
+    return (
+        <div>
+            <div className="social-handles"></div>
+        </div>
+    );
+};
+
+export default Handles;
---
+++ digital-card/src/components/Info.tsx
@@ -0,0 +1,42 @@
+import "./Info.css";
+
+const Info = () => {
+    return (
+        <main>
+            <div className="image"></div>
+            <div className="rectangle">
+                <div className="info">
+                    <h3 className="name">Shrijal Acharya</h3>
+                    <h4 className="job-title">Aspiring Software Engineer</h4>
+                    <h5 className="website">
+                        <a href="https://shricodev.github.io" target={"_blank"}>
+                            shricodev.github.io
+                        </a>
+                    </h5>
+                    <div className="linkedin-email">
+                        <button className="email">
+                            <i className="fa-sharp fa-solid fa-envelope fa-lg"></i>
+                            <a
+                                href="mailto:shrijal.acharya@gmail.com"
+                                target={"_blank"}
+                            >
+                                Gmail
+                            </a>
+                        </button>
+                        <button className="linkedin">
+                            <i className="fa-brands fa-linkedin-in fa-lg"></i>
+                            <a
+                                href="https://linkedin.com/in/shrijal007"
+                                target={"_blank"}
+                            >
+                                LinkedIn
+                            </a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Info;
