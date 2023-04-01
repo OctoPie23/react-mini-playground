efdf2b3ef5b3686dbc2aec57f42aae65f81af5fb
Shrijal Acharya <coveringguy0123@gmail.com>
2023-04-01 14:18:20 +0545

    Add the 'Navbar' component to 'travel-journal'

---
+++ travel-journal/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="mb-[50px] flex w-full min-w-[500px] items-center justify-center bg-blue-600 py-5">
+            <i className="fa-solid fa-rocket lg:text-4xl: pr-3 text-teal-300 sm:text-xl md:text-2xl"></i>
+            <h1 className="font-bold text-gray-100 sm:text-xl md:text-2xl lg:text-3xl">
+                My Travel Journal
+            </h1>
+        </nav>
+    );
+};
+
+export default Navbar;
