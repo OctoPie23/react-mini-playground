111e670e290ba1047914f192a06ecfd446797990
Shrijal Acharya <coveringguy0123@gmail.com>
2023-04-01 14:49:27 +0545

    Add the 'Card' component to 'travel-journal'

--- travel-journal/src/App.tsx
+++ travel-journal/src/App.tsx
@@ -1,9 +1,28 @@
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import TravelData from "./components/TravelData";
+
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <Navbar />
+            {TravelData.map((card) => {
+                return (
+                    <Card
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        location={card.location}
+                        googleMapsUrl={card.googleMapsUrl}
+                        startDate={card.startDate}
+                        endDate={card.endDate}
+                        description={card.description}
+                        imageUrl={card.imageUrl}
+                    />
+                );
+            })}
         </div>
     );
 }
